f4fb4462adcd0eb5ef81a1b92f34b2d3a3c098e2
Batyrzhan2002 <batyrzhantogaev2002@icloud.com>
2026-08-29 16:08:06 +0500

    Configured the project configuration

---
+++ config.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from pathlib import Path
+from datetime import datetime
+
+MAX_LIMIT_DAY = 14
+LATE_PAYMENT_PENALTY = 10
+MAX_LIMIT_BOOKS = 3
+FORMAT_FOR_DATE = '%d.%m.%Y'
+
+MAIN_FILE_ADDRESS = Path('data/')
+MAIN_FILE_ADDRESS.mkdir(parents=True, exist_ok=True)
+JSON_BOOK_ADDRESS = MAIN_FILE_ADDRESS + 'books.json'
+JSON_LOAN_ADDRESS = MAIN_FILE_ADDRESS + 'loans.json'
+JSON_READER_ADDRESS = MAIN_FILE_ADDRESS +'readers.json'
+
+class GenresBooks(Enum):
+    FANTASY = 'Фантастика'
+    CLASSIC = 'Классика'
+    DETECTIVE = 'Детектив'
+
+
+class LoanStatus(Enum):
+    ACTIVE = 'Активен'
+    RETURNED = 'Возвращен'
+    OVERDUE = 'Просрочен'
