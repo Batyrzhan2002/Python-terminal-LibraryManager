8e44c003aa51e705d7ee9cae370fe7e0773580d9
Batyrzhan2002 <batyrzhantogaev2002@icloud.com>
2026-09-17 23:09:38 +0500

    complete reader_repositories, add full funcion

--- repositories/reader_repositories.py
+++ repositories/reader_repositories.py
@@ -18,3 +18,53 @@ class ReaderRepositories:
         else:
             with open(self.file_path, 'w', encoding='utf-8') as file:
                 json.dump([], file)
+
+
+
+    def save(self):
+        data = []
+        for reader in self.readers:
+            data.append(reader.to_dict())
+
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+    def get_all(self):
+        return self.readers.copy()
+
+
+    def get_by_id(self, id):
+        for reader in self.readers:
+            if reader.id == id:
+                return reader
+
+        return None
+
+
+    def add(self, reader):
+        new_id = max((item.id for item in self.readers), default=0) + 1
+        reader.id = new_id
+        self.readers.append(reader)
+        self.save()
+        return reader
+
+
+    def update(self, reader):
+        for i, item in enumerate(self.readers):
+            if item.id == reader.id:
+                self.readers[i] = reader
+                self.save()
+                return True
+
+        return False
+
+
+    def delete(self, reader_id):
+        for i, item in enumerate(self.readers):
+            if item.id == reader_id:
+                self.readers.pop(i)
+                self.save()
+                return True
+
+        return False
